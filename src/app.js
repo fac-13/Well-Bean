a23416dddd27f9cee082027591f16b06571c1787
Nicos <nick.zonias@gmail.com>
2018-05-09 17:12:42 +0100

    add comments for app.js and index.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+// require controllers
 const controllers = require('./controllers');
 
 const app = express();
 
+// set up server
 app.use(controllers);
 
 app.set('port', process.env.PORT || 3000);
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
+
+// require controllers
 const home = require('./home');
 const challenges = require('./challenges');
 
+// set up routes
 router.get('/', home.get);
 router.get('/challenges', challenges.get);
 
