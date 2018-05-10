4573e738e572c3d81f30388b9239e124fef7f99c
i2xzy <yitzchokr@live.com>
2018-05-10 16:04:20 +0100

    add controller for post user_challenge relates #15

--- src/test/test.js
+++ src/test/test.js
@@ -7,24 +7,56 @@ test('test tape', (t) => {
   t.end();
 });
 
-test('Test home route running', (t) => {
+test('Test home route', (t) => {
   request(router)
     .get('/')
     .expect(200)
+    .expect('Content-Type', /html/)
     .end((err, res) => {
       t.error(err);
+      t.ok(res.text.includes('<html>'), 'response is an html file');
       t.end();
     });
 });
 
-test('Test challenges route running', (t) => {
+test('Test GET challenges list view route', (t) => {
   request(router)
     .get('/challenges')
     .expect(200)
     .expect('Content-Type', /html/)
     .end((err, res) => {
-      t.ok(res.text.includes('Title 1'), 'Test returns expected text');
       t.error(err);
+      t.ok(res.text.includes('Title 1'), 'response contains entry from list');
       t.end();
     });
 });
+
+test('Test GET challenge detail view route', (t) => {
+  request(router)
+    .get('/challenge/1')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(
+        res.text.includes('Morning'),
+        'response contains info about individual challenge',
+      );
+      t.end();
+    });
+});
+
+test('Test select challenge POST route', (t) => {
+  request(router)
+    .post('/user-challenge/1')
+    .expect(302)
+    .end((err, res) => {
+      t.error(err);
+      t.ok(res, 'response has something from query');
+      t.end();
+    });
+});
+
+// **********************************************
+// **************database tests******************
+// **********************************************
