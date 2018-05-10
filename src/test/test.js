07186276418a8c3829ef7904e8bb59b4b5d930ef
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-10 15:07:18 +0100

    create challenge query test Relates #2 #4 & #13

--- src/test/test.js
+++ src/test/test.js
@@ -1,6 +1,8 @@
 const test = require('tape'); //eslint-disable-line
 const request = require('supertest'); //eslint-disable-line
 const router = require('./../app.js');
+// require test databse build script
+const runDbBuild = require('../model/database/db_build');
 
 test('test tape', (t) => {
   t.pass('tape is working');
@@ -28,3 +30,16 @@ test('Test challenges route running', (t) => {
       t.end();
     });
 });
+
+test('Test for the first row of challenges query', (t) => {
+  const expected = {
+    id: 1, categories_id: 2, user_id: null, title: 'Morning Hydration', description: 'Drink water in the morning to hydrate your body. People don\'t realise you sleep for approximately 8 hours without drinking water. Your body is dehydrated in the morning, get a good kickstart to the day!',
+  };
+  runDbBuild((err, res) => {
+    getAllChallenges().then((res) => {
+      t.deepEqual(res[0], expected, 'getAllChallenges returns first challenge in table');
+      t.end();
+    });
+  });
+});
+
