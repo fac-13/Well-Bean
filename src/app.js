da4b9058f43ef8fe089b7f9243cfdd4aecf11acf
i2xzy <yitzchokr@live.com>
2018-05-10 11:06:15 +0100

    render challenges with dummy data relates #13

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 // require controllers
 const controllers = require('./controllers');
 
 const app = express();
 
+// set up views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+  }),
+);
+
 // set up server
 app.use(controllers);
-
 app.set('port', process.env.PORT || 3000);
 
 module.exports = app;
