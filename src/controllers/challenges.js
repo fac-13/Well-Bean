6899fb423e7dc3e609773bdfbb716c021887a5cc
HA <haydnba@gmail.com>
2018-05-10 12:50:02 +0100

    add more dummy data and template formatting Relates #5

--- src/controllers/challenges.js
+++ src/controllers/challenges.js
@@ -2,10 +2,22 @@ const challenges = [
   {
     title: 'Title 1',
     description: 'Description 1',
+    username: 'Tinky Winky',
   },
   {
     title: 'Title 2',
     description: 'Description 2',
+    username: 'LaaLaa',
+  },
+  {
+    title: 'Title 3',
+    description: 'Description 3',
+    username: 'Po',
+  },
+  {
+    title: 'Title 4',
+    description: 'Description 4',
+    username: 'Dipsy',
   },
 ];
 
