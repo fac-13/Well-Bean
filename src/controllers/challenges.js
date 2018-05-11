0e02bdd6ae0ba417bb30fb6c832b335f24fa67c4
HA <haydnba@gmail.com>
2018-05-11 11:58:16 +0100

    minor fix

--- src/controllers/challenges.js
+++ src/controllers/challenges.js
@@ -1,4 +1,4 @@
-const { getAllChallenges } = require('./../model/queries');
+const { getAllChallenges } = require('./../model/queries/index');
 
 exports.get = (req, res) => {
   getAllChallenges().then(challenges => res.render('challenges', { challenges }));
