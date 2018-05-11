f1e06d3501ea90c344af8856d995ea9d7fc76b10
i2xzy <yitzchokr@live.com>
2018-05-11 15:00:10 +0100

    undo unnecessary change

--- src/controllers/user_challenge.js
+++ src/controllers/user_challenge.js
@@ -2,7 +2,7 @@ const { postUserChallenge } = require('../model/queries/');
 
 exports.post = (req, res) => {
   const userId = 1;
-  const challengeId = parseInt(req.params.id, 10);
+  const challengeId = req.params.id;
 
   postUserChallenge(userId, challengeId)
     .then(() => {
