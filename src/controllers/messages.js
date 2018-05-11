daa26ff26349810291b3afe10e4a62776401ff6f
Nicos <nick.zonias@gmail.com>
2018-05-11 12:38:42 +0100

    add more details to messages view Relates to #2, #10, #26

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,17 +1,9 @@
 const getMessages = require('./../model/queries/get_messages.js');
-// [
-//   {
-//     id: 1,
-//     body: 'Have a conversation with the sun',
-//     username: 'TinkyWinky',
-//     added: '10/05/2018',
-//   }
-// ];
 
 exports.get = (req, res) => {
   getMessages()
-    .then(allMessages => res.render('messages', {
-      allMessages,
-    }))
-    .catch(e => console.log(e));
+    .then(allMessages =>
+      res.render('messages', {
+        allMessages,
+      }));
 };
--- src/model/queries/get_messages.js
+++ src/model/queries/get_messages.js
@@ -1,7 +1,7 @@
 const db = require('./../database/db_connection');
 
 const getMessages = () => db.query(`
-    SELECT m.id, m.user_id, m.body, to_char(m.added, 'Month DD, YYYY at HH12:MI pm') AS time, u.username
+    SELECT m.id, m.body, to_char(m.added, 'Month DD, YYYY at HH12:MI pm') AS time, u.username
     FROM messages AS m
     LEFT JOIN users AS u ON m.user_id = u.id
     ORDER BY m.id desc
