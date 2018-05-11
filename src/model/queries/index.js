085c94de636e48deac6f425c84ac12b29db89abd
Nicos <nick.zonias@gmail.com>
2018-05-11 12:48:47 +0100

    add getMessages to queries index file

--- src/model/queries/index.js
+++ src/model/queries/index.js
@@ -1,7 +1,9 @@
 const getAllChallenges = require('./get_all_challenges');
 const getChallenge = require('./get_challenge');
+const getMessages = require('./get_messages');
 
 module.exports = {
   getAllChallenges,
   getChallenge,
+  getMessages,
 };
