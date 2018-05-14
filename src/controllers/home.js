c1ecf526896478cb7576d1cd3acec2d945bf3177
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-14 13:05:31 +0100

    display results on home handlebar #32 & #29

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,7 @@
-exports.get = (req, res) => {
-  res.render('home');
+const { getActiveChallenge } = require('../model/queries/');
+
+exports.get = (req, res, next) => {
+  getActiveChallenge(1)
+    .then(activeChallenge => res.render('home', activeChallenge[0]))
+    .catch(e => next(e));
 };
