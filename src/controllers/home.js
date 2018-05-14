a859c7c46207a210e8dcf4dfbc8e3e60d75fbab6
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-14 15:01:47 +0100

    add const userId to home controller Relates #29 & #32

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,9 @@
 const { getActiveChallenge } = require('../model/queries/');
 
+
 exports.get = (req, res, next) => {
-  getActiveChallenge(1)
+  const userId = 1; // needs to be changed for cookie id
+  getActiveChallenge(userId)
     .then(activeChallenge => res.render('home', activeChallenge[0]))
     .catch(e => next(e));
 };
