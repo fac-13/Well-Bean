06c5542b612a10f9018338759d860d5fc1bfcfca
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-14 12:32:40 +0100

    refactor queries for consistency Relates #32 & #29

--- src/model/queries/get_active_challenge.js
+++ src/model/queries/get_active_challenge.js
@@ -1,10 +1,13 @@
 const db = require('../database/db_connection');
 
 const getActiveChallenges = userId =>
-  db.query(`SELECT uc.id, ch.title, cat.name AS category FROM challenges AS ch
+  db.query(`
+  SELECT uc.id, ch.title, cat.name AS category 
+  FROM challenges AS ch
   LEFT JOIN categories AS cat ON ch.categories_id = cat.id
   INNER JOIN user_challenges AS uc
   ON ch.id = uc.challenges_id
-  WHERE uc.user_id = $1 AND uc.status = 'active';`, [userId]);
+  WHERE uc.user_id = $1 AND uc.status = 'active';
+  `, [userId]);
 
 module.exports = getActiveChallenges;
--- src/model/queries/post_user_challenge.js
+++ src/model/queries/post_user_challenge.js
@@ -1,11 +1,10 @@
 const db = require('../database/db_connection');
 
 const postUserChallenge = (userId, challengeId) =>
-  db.query(
-    `INSERT INTO user_challenges (user_id, challenges_id)
-    VALUES ($1, $2)
-    RETURNING id;`,
-    [userId, challengeId],
-  );
+  db.query(`
+  INSERT INTO user_challenges (user_id, challenges_id)
+  VALUES ($1, $2)
+  RETURNING id;
+  `, [userId, challengeId]);
 
 module.exports = postUserChallenge;
