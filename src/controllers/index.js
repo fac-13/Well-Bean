58150ce85c6f6bf086626f337258003c2cea140c
HA <haydnba@gmail.com>
2018-05-15 11:42:51 +0100

    add failing test for signup post route Relates #19

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,7 +9,6 @@ const challenges = require('./challenges');
 const challenge = require('./challenge');
 const addChallenge = require('./add_challenge');
 const userChallenge = require('./user_challenge');
-
 const updateChallenge = require('./update_challenge');
 const messages = require('./messages');
 const error = require('./error');
@@ -25,6 +24,7 @@ router.get('/messages', messages.get);
 router.post('/user-challenge/:id', userChallenge.post); // select a challenge
 router.post('/update-challenge/:status/:id', updateChallenge.post);
 router.post('/add-challenge', addChallenge.post);
+router.post('/signup', signup.post);
 
 router.use(error.client);
 router.use(error.server);
--- src/test/controllers/signup.test.js
+++ src/test/controllers/signup.test.js
@@ -1,8 +1,10 @@
 const test = require('tape'); //eslint-disable-line
 const request = require('supertest'); //eslint-disable-line
 const router = require('../../app.js');
+// require test database build script
+const runDbBuild = require('../../model/database/db_build');
 
-test('Test signup route', (t) => {
+test('Test signup GET route', (t) => {
   request(router)
     .get('/signup')
     .expect(200)
@@ -11,6 +13,30 @@ test('Test signup route', (t) => {
       t.ok(res);
       t.error(err);
       t.ok(res.text.includes('Signup'), 'response contains signup message');
+      t.end();
+    });
+});
+
+test('Test signup POST route', (t) => {
+  runDbBuild()
+    .then((dbRes) => {
+      t.ok(dbRes, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'haydn',
+          password: 'madskillz',
+          email: 'email@email.com',
+        })
+        .expect(302)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res, 'response has something from query');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
       t.end();
     });
 });
