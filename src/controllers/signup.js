317b59128e21d2b70d86ba3d348705c19bad4020
HA <haydnba@gmail.com>
2018-05-15 11:54:16 +0100

    refactor post signup controller and complete basic signup functionality Relates #19

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,1 +1,11 @@
+const { postNewUser } = require('../model/queries/');
+
 exports.get = (req, res) => res.render('signup');
+
+exports.post = (req, res, next) => {
+  const { username, password, email } = req.body;
+
+  postNewUser(username, password, email)
+    .then(() => res.redirect('/'))
+    .catch(e => next(e));
+};
