68569ca30b0b32095224aee47aa53b144928556e
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-16 11:53:54 +0100

    100% coverage for home test

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,7 +2,7 @@ const { getActiveChallenge } = require('../model/queries/');
 
 
 exports.get = (req, res, next) => {
-  const userId = 1; // needs to be changed for cookie id
+  const userId = req.headers.userid; // needs to be changed for cookie id
   getActiveChallenge(userId)
     .then(activeChallenge => res.render('home', activeChallenge[0]))
     .catch(e => next(e));
