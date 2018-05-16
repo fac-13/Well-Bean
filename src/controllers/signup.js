1fe74b509b1cc58bcfee225ae20106fb0b74525e
Nicos <nick.zonias@gmail.com>
2018-05-16 14:33:49 +0100

    hash passwords Relates to #7 #19

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,13 +1,19 @@
 const { postNewUser } = require('../model/queries/');
+const bcrypt = require('bcrypt');
+
 
 exports.get = (req, res) => res.render('signup');
 
 exports.post = (req, res, next) => {
+  const saltRounds = 10;
   const { username, password, email } = req.body;
   if (username && password && email) {
-    postNewUser(username, password, email)
-      .then(() => res.redirect('/'))
-      .catch(e => next(e));
+    bcrypt.hash(password, saltRounds)
+      .then(hash =>
+        postNewUser(username, hash, email)
+          .then(() => res.redirect('/'))
+          .catch(e => next(e)))
+      .catch(err => next(err));
   } else if (!username) {
     res.render('signup', { error: 'username' });
   } else if (!email) {
