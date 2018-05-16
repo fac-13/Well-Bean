54d85aec1b607a1d9100798a43011e956d72df33
i2xzy <yitzchokr@live.com>
2018-05-16 17:06:12 +0100

    add equals helper, display selected challenge on rerender

--- src/views/helpers/equals.js
+++ src/views/helpers/equals.js
@@ -1,5 +1,4 @@
 module.exports = (lvalue, rvalue, options) => {
-  console.log(lvalue, rvalue);
   if (arguments.length < 3) { throw new Error('Handlebars Helper equal needs 2 parameters'); }
   return (lvalue === rvalue) ? options.fn(this) : options.inverse(this);
 };
