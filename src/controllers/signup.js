1736d823198be61d3224ec532ceaee968de0f456
Nicos <nick.zonias@gmail.com>
2018-05-17 07:25:42 +0100

    more error warnings signup Relates #7 #19

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -6,25 +6,39 @@ exports.get = (req, res) => res.render('signup');
 
 exports.post = (req, res, next) => {
   const saltRounds = 10;
-  const { username, password, email } = req.body;
-  if (username && password && email) {
-    bcrypt.hash(password, saltRounds)
-      .then(hash =>
-        postNewUser(username, hash, email)
-          .then(() => res.redirect('/'))
-          .catch((e) => {
-            if (e.detail.includes('already exists')) {
-              console.log(e.detail);
-              res.render('signup', {
-                usernameValue: username,
-                emailValue: email,
-                errorText: 'Username already taken. Please use login to your account or choose a new username',
-              });
-            } else {
-              next(e);
-            }
-          }))
-      .catch(err => next(err));
+  const {
+    username, password, email, confirm_password: confirmPassword,
+  } = req.body;
+  if (username && password && email && confirmPassword) {
+    if (password !== confirmPassword) {
+      res.render('signup', {
+        usernameValue: username,
+        emailValue: email,
+        errorText: 'Your passwords don\'t match! Please try again',
+      });
+    } else {
+      bcrypt.hash(password, saltRounds)
+        .then(hash =>
+          postNewUser(username, hash, email)
+            .then(() => res.redirect('/'))
+            .catch((e) => {
+              if (e.detail.includes(`(${username}) already exists.`)) {
+                res.render('signup', {
+                  usernameValue: username,
+                  emailValue: email,
+                  errorText: 'Username already taken. Please login to your account or choose a new username',
+                });
+              } else if (e.detail.includes(`(${email}) already exists.`)) {
+                res.render('signup', {
+                  usernameValue: username,
+                  errorText: 'Email already registered. Please login to your account instead',
+                });
+              } else {
+                next(e);
+              }
+            }))
+        .catch(err => next(err));
+    }
   } else {
     res.render('signup', {
       usernameValue: username || null,
--- src/model/queries/index.js
+++ src/model/queries/index.js
@@ -2,7 +2,7 @@ const getAllChallenges = require('./get_all_challenges');
 const getChallenge = require('./get_challenge');
 const getMessages = require('./get_messages');
 const getActiveChallenge = require('./get_active_challenge');
-
+const getUser = require('./get_user');
 const postChallenge = require('./post_challenge');
 const postMessage = require('./post_message');
 const postUserChallenge = require('./post_user_challenge');
@@ -15,6 +15,7 @@ module.exports = {
   getActiveChallenge,
   getChallenge,
   getMessages,
+  getUser,
   postChallenge,
   postMessage,
   postUserChallenge,
