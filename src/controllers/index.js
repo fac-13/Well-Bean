847495b8ba553a38c8062a120ce8635ab4c92443
HA <haydnba@gmail.com>
2018-05-17 23:32:11 +0100

    add reporting routes and get report controller, test passing Relates #33 #34 #35

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -15,9 +15,10 @@ const progress = require('./progress');
 const updateChallenge = require('./update_challenge');
 const messages = require('./messages');
 const addMessage = require('./add_message');
+const report = require('./report');
 const error = require('./error');
 
-// set up routes
+// set up GET routes
 router.get('/', home.get);
 router.get('/signup', signup.get); // get signup form
 router.get('/login', login.get); // get login form
@@ -28,13 +29,16 @@ router.get('/add-challenge', addChallenge.get); // get addChallenge form
 router.get('/messages', messages.get);
 router.get('/add-message', addMessage.get); // get addMessage form
 router.get('/progress', progress.get); // get all user challenges
+router.get('/report', report.get);
 
+// Set up POST routes
+router.post('/signup', signup.post);
+router.post('/login', login.post);
 router.post('/user-challenge/:id', userChallenge.post); // select a challenge
 router.post('/update-challenge/:status/:id', updateChallenge.post); // update user-challenge
 router.post('/add-challenge', addChallenge.post);
 router.post('/add-message', addMessage.post);
-router.post('/signup', signup.post);
-router.post('/login', login.post);
+// router.post('/report', report.post);
 
 router.use(error.client);
 router.use(error.server);
