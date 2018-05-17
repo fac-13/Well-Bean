95b8ffd5424c00f3751162f619fe5384aab48b54
Nicos <nick.zonias@gmail.com>
2018-05-17 19:03:57 +0100

    more tests of progress handlebar Relates to #9

--- src/controllers/progress.js
+++ src/controllers/progress.js
@@ -3,7 +3,6 @@ const { getUserChallenges } = require('../model/queries/');
 exports.get = (req, res, next) => {
   const { userId, userName } = req.session;
   getUserChallenges(userId)
-    .then()
     .then(userChallenges => res.render('progress', { userChallenges, userName }))
     .catch(e => next(e));
 };
