269b8cdd803f9c163e9e2e53d128ad9d9cc6e7d1
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-17 14:34:54 +0100

    attach userId and loggedIn = true to session cookie Relates #18 #7

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -13,7 +13,11 @@ exports.post = (req, res, next) => {
       bcrypt.hash(password, 10)
         .then(hash =>
           postNewUser(username, hash, email)
-            .then(() => res.redirect('/'))
+            .then((newUser) => {
+              req.session.userId = newUser[0].id;
+              req.session.loggedIn = true;
+              res.redirect('/');
+            })
             .catch((e) => {
               if (e.detail.includes(email)) {
                 res.render('signup', {
