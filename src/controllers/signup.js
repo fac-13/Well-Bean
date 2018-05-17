28222c4d2abf9000083caedbbcc3ad83be38b30b
Nicos <nick.zonias@gmail.com>
2018-05-17 11:50:39 +0100

    test coverage for signup Relates to #7 #19

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -5,45 +5,43 @@ const bcrypt = require('bcrypt');
 exports.get = (req, res) => res.render('signup');
 
 exports.post = (req, res, next) => {
-  const saltRounds = 10;
   const {
-    username, password, email, confirm_password: confirmPassword,
+    username, password, email, confirmPassword,
   } = req.body;
-  if (username && password && email && confirmPassword) {
-    if (password !== confirmPassword) {
-      res.render('signup', {
-        usernameValue: username,
-        emailValue: email,
-        errorText: 'Your passwords don\'t match! Please try again',
-      });
-    } else {
-      bcrypt.hash(password, saltRounds)
+  if (username && password && email) {
+    if (password === confirmPassword) {
+      bcrypt.hash(password, 10)
         .then(hash =>
           postNewUser(username, hash, email)
             .then(() => res.redirect('/'))
             .catch((e) => {
-              if (e.detail.includes(`(${username}) already exists.`)) {
-                res.render('signup', {
-                  usernameValue: username,
-                  emailValue: email,
-                  errorText: 'Username already taken. Please login to your account or choose a new username',
-                });
-              } else if (e.detail.includes(`(${email}) already exists.`)) {
+              if (e.detail.includes(email)) {
                 res.render('signup', {
                   usernameValue: username,
                   errorText: 'Email already registered. Please login to your account instead',
+                });
+              } else if (e.detail.includes(username)) {
+                res.render('signup', {
+                  emailValue: email,
+                  errorText: 'Username already taken. Please login to your account or choose a new username',
                 });
               } else {
                 next(e);
               }
             }))
-        .catch(err => next(err));
+        .catch(e => next(e));
+    } else {
+      res.render('signup', {
+        usernameValue: username,
+        emailValue: email,
+        errorText: 'Your passwords don\'t match! Please try again',
+      });
     }
   } else {
     res.render('signup', {
       usernameValue: username || null,
       emailValue: email || null,
-      errorText: !username || !email || !password ? 'Please enter your username, your email and a valid password, can not be empty' : null,
+      errorText: !username || !email || !password ? 'Please enter your username, your email and a valid password, can not be empty' : '',
     });
   }
 };
--- src/test/controllers/signup.test.js
+++ src/test/controllers/signup.test.js
@@ -26,8 +26,9 @@ test('Test signup POST route - new user', (t) => {
           username: 'haydn',
           password: 'madskillz',
           email: 'email@email.com',
+          confirmPassword: 'madskillz',
         })
-        .expect(200)
+        .expect(302)
         .end((err, res) => {
           t.error(err);
           t.ok(res, 'new user successfully signed up');
@@ -41,7 +42,7 @@ test('Test signup POST route - new user', (t) => {
 });
 
 
-test('Test signup POST route - existing user', (t) => {
+test('Test signup POST route - username already exists', (t) => {
   runDbBuild()
     .then((dbResponse) => {
       t.ok(dbResponse, 'database built');
@@ -51,12 +52,136 @@ test('Test signup POST route - existing user', (t) => {
           username: 'Dipsy',
           email: 'dipsy@winky.com',
           password: 'password123',
-          confirmPassword: 'password12',
+          confirmPassword: 'password123',
         })
         .expect(200)
-        .end((error, response) => {
-          t.error(error);
-          t.ok(response, 'warning issued that user exists');
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('Username already taken'), 'warning issued that user exists');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
+test('Test signup POST route - email already exists', (t) => {
+  runDbBuild()
+    .then((dbResponse) => {
+      t.ok(dbResponse, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'Dip',
+          email: 'dipsy@winky.com',
+          password: 'password123',
+          confirmPassword: 'password123',
+        })
+        .expect(200)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('Email already'), 'warning issued that email exists');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
+
+test('Test signup POST route - passwoards don\'t match', (t) => {
+  runDbBuild()
+    .then((dbResponse) => {
+      t.ok(dbResponse, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'Dipsy',
+          email: 'dipsy@winky.com',
+          password: 'password123',
+          confirmPassword: 'password1',
+        })
+        .expect(200)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('passwords'), 'warning issued that passwords don\'t match');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
+
+test('Test signup POST route - confirmed password empty', (t) => {
+  runDbBuild()
+    .then((dbResponse) => {
+      t.ok(dbResponse, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'Dipsy',
+          email: 'dipsy@winky.com',
+          password: 'password123',
+        })
+        .expect(200)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('passwords'), 'warning issued that passwords don\'t match');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
+test('Test signup POST route - empty fields', (t) => {
+  runDbBuild()
+    .then((dbResponse) => {
+      t.ok(dbResponse, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'Dipsy',
+        })
+        .expect(200)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('can not be empty'), 'warning issued that fields are missing');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
+
+test('Test signup POST route - wrong password type', (t) => {
+  runDbBuild()
+    .then((dbResponse) => {
+      t.ok(dbResponse, 'database built');
+      request(router)
+        .post('/signup')
+        .send({
+          username: 'Dipsy',
+          email: 'dipsy@winky.com',
+          password: true,
+          confirmPassword: true,
+        })
+        .expect(500)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res.text.includes('500'), '500 warning issued');
           t.end();
         });
     })
