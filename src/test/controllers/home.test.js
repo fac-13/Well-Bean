bfd8795f5674ff54691aa0ee3b67d2ebd988c0c4
i2xzy <yitzchokr@live.com>
2018-05-17 16:42:39 +0100

    add add-message button on messages page relates #27 #28

--- src/test/controllers/home.test.js
+++ src/test/controllers/home.test.js
@@ -2,6 +2,30 @@ const test = require('tape'); //eslint-disable-line
 const request = require('supertest'); //eslint-disable-line
 const router = require('../../app.js');
 
+
+test('Test login POST route - good email', (t) => {
+  runDbBuild()
+    .then((dbRes) => {
+      t.ok(dbRes, 'database built');
+      request(router)
+        .post('/login')
+        .send({
+          inputUser: 'tinky@winky.com',
+          inputPassword: 'password123',
+        })
+        .expect(302)
+        .end((err, res) => {
+          t.error(err);
+          t.ok(res, 'succesfully redirected to homepage');
+          t.end();
+        });
+    })
+    .catch((e) => {
+      t.error(e);
+      t.end();
+    });
+});
+
 test('Test home route', (t) => {
   request(router)
     .get('/')
