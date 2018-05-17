044ceb028352d4ba74f2f0cacc3501875a7911e4
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-17 20:17:06 +0100

    attempt at cookie test

--- src/test/controllers/home.test.js
+++ src/test/controllers/home.test.js
@@ -13,3 +13,36 @@ test('Test home route with loggedIn false', (t) => {
       t.end();
     });
 });
+
+// Auxiliary function.
+function createLoginCookie(server, loginDetails, done) {
+  request(router)
+    .post(server)
+    .send(loginDetails)
+    .end((error, response) => {
+      if (error) {
+        console.log(error);
+      }
+      const loginCookie = response.session;
+      done(loginCookie);
+    });
+}
+
+// Using auxiliary function in test cases.
+
+test.only('Test home route with loggedIn false', (t) => {
+  createLoginCookie('/login', {
+    inputUser: 'tinky@winky.com',
+    inputPassword: 'password123',
+  }, (cookie) => {
+    request(router)
+      .get('/')
+      .set('cookie', cookie)
+      .expect(302)
+      .end((err, res) => {
+        t.error(err);
+        t.ok(res.text.includes('/', 'redirects correctly'));
+        t.end();
+      });
+  });
+});
