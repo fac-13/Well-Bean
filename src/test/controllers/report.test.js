eca4a967ae3ecb2141635afb47cdb451b5316a9c
HA <haydnba@gmail.com>
2018-05-18 11:15:28 +0100

    have to finish now! #33 #34 #35

--- src/test/controllers/report.test.js
+++ src/test/controllers/report.test.js
@@ -2,7 +2,7 @@ const test = require('tape'); //eslint-disable-line
 const request = require('supertest'); //eslint-disable-line
 const router = require('../../app.js');
 
-test.only('Test report GET route', (t) => {
+test('Test report GET route', (t) => {
   request(router)
     .get('/report?challenge=3')
     .expect(200)
