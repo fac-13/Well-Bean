70f860e32ee89408ac3912d1118556d23a258012
HA <haydnba@gmail.com>
2018-05-19 09:29:13 +0100

    add referrer data for accessible navigation from chllenge detail view page Relates #33 #34 #35

--- src/controllers/challenge.js
+++ src/controllers/challenge.js
@@ -1,10 +1,13 @@
 const { getChallenge } = require('../model/queries/');
 
 exports.get = (req, res, next) => {
-  getChallenge(req.params.id)
+  const { referrer } = req.query;
+  const { id: challengeId } = req.params;
+  getChallenge(challengeId)
     .then((challenges) => {
       const challenge = challenges[0];
       challenge.state = { challenges: true };
+      challenge.referrer = referrer;
       res.render('challenge', challenge);
     })
     .catch(e => next(e));
